3fdb6c084ee76208b08f1c345d11ada183881641
fzh <fzhzhen1987@yahoo.co.jp>
2023-09-08 13:39:32 +0900

    更改插件按键whichkey.lua

--- lua/FZH_lua/configs/whichkey.lua
+++ lua/FZH_lua/configs/whichkey.lua
@@ -12,10 +12,11 @@ end
 
 local G = require('FZH_lua.G')
 
+-- 完全使用新版本语法的配置
 local setup = {
 	plugins = {
-		marks = ture, -- shows a list of your marks on ' and `
-		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+		marks = true, -- shows a list of your marks on ' and `
+		registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
 		spelling = {
 			enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
 			suggestions = 20, -- how many suggestions should be shown in the list?
@@ -32,124 +33,102 @@ local setup = {
 			g = false, -- bindings for prefixed with g
 		},
 	},
-	-- add operators that will trigger motion and text object completion
-	-- to enable all native operators, set the preset / operators plugin above
-	-- operators = { gc = "Comments" },
-	key_labels = {
-		-- override the label used to display some keys. It doesn't effect WK in any other way.
-		-- For example:
-		-- ["<space>"] = "SPC",
-		-- ["<cr>"] = "RET",
-		-- ["<tab>"] = "TAB",
-	},
 	icons = {
 		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
 		separator = "➜", -- symbol used between a key and it's label
 		group = "+", -- symbol prepended to a group
 	},
-	popup_mappings = {
+	-- 新版本语法：使用 keys 替代 popup_mappings
+	keys = {
 		scroll_down = "<C-j>", -- binding to scroll down inside the popup
 		scroll_up = "<C-k>", -- binding to scroll up inside the popup
 	},
-	window = {
-		border = "rounded", -- none, single, double, shadow
-		position = "bottom", -- bottom, top
-		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-		winblend = 0,
-	},
 	layout = {
-		height = { min = 4, max = 25 }, -- min and max height of the columns
-		width = { min = 20, max = 50 }, -- min and max width of the columns
-		spacing = 3, -- spacing between columns
-		align = "left", -- align columns left, center or right
+		height = { min = 4, max = 25 }, -- 行高度范围
+		width = { min = 40, max = 120 }, -- 列宽度范围，增加到几乎全屏宽度
+		spacing = 5, -- 列之间的间距
+		align = "left", -- 对齐方式
 	},
-	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+	-- 新版本语法：使用 filter 替代 ignore_missing
+	filter = function(mapping)
+		-- 返回 true 表示显示这个映射
+		return true
+	end,
 	show_help = true,
 	show_keys = true,
-	triggers = "auto", -- automatically setup triggers
-	-- triggers = {"<leader>"} -- or specify a list manually
-	triggers_blacklist = {
-		-- list of mode / prefixes that should never be hooked by WhichKey
-		-- this is mostly relevant for key maps that start with a native binding
-		-- most people should not need to change this
-		i = {  },
-		v = {  },
-	},
+	-- 新版本可能不需要 triggers，如果出错可以注释掉
+	-- triggers = { "<auto>" }, -- automatically setup triggers
 }
 
-local n_opts = {
-	mode = "n", -- NORMAL mode
-	prefix = "",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
+-- 新版本映射格式：所有映射合并到一个数组中
+local all_mappings = {
+	-- ================== LSP 核心功能 ==================
+	-- === 导航功能 ===
+	{ "gd", "<cmd>lua smart_telescope.smart_lsp_definitions()<cr>", desc = "📍 LSP: 跳转到定义", nowait = true, remap = false },
+	{ "gD", "<cmd>lua smart_telescope.smart_lsp_declarations()<cr>", desc = "📌 LSP: 跳转到声明", nowait = true, remap = false },
+	{ "gi", "<cmd>lua smart_telescope.smart_lsp_implementations()<cr>", desc = "⚡ LSP: 查找实现", nowait = true, remap = false },
+	{ "gt", "<cmd>lua smart_telescope.smart_lsp_type_definitions()<cr>", desc = "🏷️ LSP: 类型定义", nowait = true, remap = false },
+
+	-- === 调用关系 ===
+	{ "gr", "<cmd>lua smart_telescope.smart_lsp_incoming_calls()<cr>", desc = "📞 LSP: 谁调用了这个函数 (入调用)", nowait = true, remap = false },
+	{ "go", "<cmd>lua smart_telescope.smart_lsp_outgoing_calls()<cr>", desc = "📤 LSP: 这个函数调用了谁 (出调用)", nowait = true, remap = false },
+	{ "gR", "<cmd>lua smart_telescope.smart_lsp_references()<cr>", desc = "🔍 LSP: 查找所有引用", nowait = true, remap = false },
+
+	-- === 符号搜索 ===
+	{ "gs", "<cmd>lua smart_telescope.smart_lsp_document_symbols()<cr>", desc = "📄 LSP: 当前文档符号", nowait = true, remap = false },
+	{ "gS", "<cmd>lua smart_telescope.smart_lsp_dynamic_workspace_symbols()<cr>", desc = "🔎 LSP: 动态工作区符号搜索", nowait = true, remap = false },
+
+	-- === 信息显示 ===
+	{ "J", "<cmd>lua vim.lsp.buf.hover()<cr>", desc = "📖 LSP: 悬浮文档 (显示详细信息)", nowait = true, remap = false },
+	{ "K", "<cmd>GitGutterPreviewHunk<CR>", desc = "👁️ Git: 预览修改块", nowait = true, remap = false },
+	{ "h", "<cmd>GitGutterPreviewHunk<CR>", desc = "👁️ Git: 预览修改块", nowait = true, remap = false },
+
+	-- === 代码操作 ===
+	{ "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "🛠️ 代码操作 (修复建议)", nowait = true, remap = false },
+	{ "gn", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "✏️ 智能重命名", nowait = true, remap = false },
+
+	-- 其他工具
+	{ "<C-r>", "<cmd>lua require('undotree').toggle()<cr>", desc = "🌲 Plugin: 撤销历史树", nowait = true, remap = false },
+
+	-- ================== Space 键映射 ==================
+	-- 代码注释
+	{ "<Space>c", "<Plug>kommentary_line_default", desc = "💬 代码注释切换", nowait = true, remap = false },
+
+	-- 代码格式化
+	--{ "<Space>gf", "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", desc = "🎨 代码格式化", nowait = true, remap = false },
+	--{ "<Space>gf", "<cmd>lua vim.lsp.buf.format({ async = true, timeout_ms = 2000, filter = function(client) return client.name == 'clangd' end })<cr>", desc = "🎨 代码格式化", nowait = true, remap = false },
+	{ "<Space>gf", "<cmd>normal! gg=G<cr>", desc = "🎨 代码格式化 (使用 Tab 缩进)", nowait = true, remap = false },
+
+	-- === 诊断功能 ===
+	{ "<Space>gq", "<cmd>lua vim.diagnostic.setloclist()<cr>", desc = "📋 LSP: 当前缓冲区诊断列表", nowait = true, remap = false },
+	{ "<Space>gQ", "<cmd>lua smart_telescope.smart_diagnostics()<cr>", desc = "🌍 所有文件诊断", nowait = true, remap = false },
+
+	-- ================== Leaderf 组 ==================
+	{ "<Space>j", group = "Leaderf & 其他 LSP 工具", nowait = true, remap = false },
+	{ "<Space>js", "<cmd>execute printf('Leaderf gtags -s %s --top --auto-preview', expand('<cword>'))<cr>", desc = "Leaderf: 符号查询", nowait = true, remap = false },
+	{ "<Space>jc", "<cmd>execute printf('Leaderf mru %s --stayOpen --preview-position cursor --no-auto-preview', expand(''))<cr>", desc = "Leaderf: 最近文件", nowait = true, remap = false },
+	{ "<Space>jl", "<cmd>execute printf('Leaderf line %s --no-sort --stayOpen --preview-position cursor --no-auto-preview', expand(''))<cr>", desc = "Leaderf: 逐行搜索", nowait = true, remap = false },
+	{ "<Space>jp", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", desc = "LSP: 显示工作区文件夹路径", nowait = true, remap = false },
+
+	-- ================== Telescope & Git 组 ==================
+	{ "<Space>g", group = "telescope 查找内容 & Git 操作", nowait = true, remap = false },
+	{ "<Space>gG", "<cmd>lua grep_global_with_highlight()<cr>", desc = "Telescope: 全局内容搜索 (支持 ripgrep 参数)", nowait = true, remap = false },
+	{ "<Space>gg", "<cmd>lua grep_word_under_cursor_with_highlight()<cr>", desc = "Telescope: 搜索光标下的词", nowait = true, remap = false },
+	{ "<Space>gp", "<cmd>GitGutterPrevHunk<CR>", desc = "Git: 上一个修改块", nowait = true, remap = false },
+	{ "<Space>gn", "<cmd>GitGutterNextHunk<CR>", desc = "Git: 下一个修改块", nowait = true, remap = false },
+	{ "<Space>gh", "<cmd>GitGutterLineHighlightsToggle<CR>", desc = "Git: 切换修改行高亮", nowait = true, remap = false },
+
+	-- ================== 可视模式映射 ==================
+	{ "<Space>c", "<Plug>kommentary_visual_default", desc = "💬 可视模式代码注释", mode = "x", nowait = true, remap = false },
+	{ "<Space>gf", "<cmd>normal! gv=<cr>", desc = "🎨 LSP: 格式化选中代码 (使用 Tab)", mode = "x", nowait = true, remap = false },
+	{ "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "🛠️ LSP: 选中代码的操作", mode = "x", nowait = true, remap = false },
+
+	-- ================== Leap 插件映射 (多模式) ==================
+	{ "F", "<Plug>(leap-backward-to)", desc = "Leap 向前查找", mode = { "x", "n", "o" }, nowait = true, remap = false },
+	{ "f", "<Plug>(leap-forward-to)", desc = "Leap 向后查找,s向前查找", mode = { "x", "n", "o" }, nowait = true, remap = false },
+	{ "<Space><Space>", "<Plug>(leap-cross-window)", desc = "Leap 全窗口跳转", mode = { "x", "n", "o" }, nowait = true, remap = false },
 }
 
-local n_mappings = {
-	--telescope Lsp 和 Leaderf
-	["<Space>j"] = { name = "Lsp 操作" },
-	["<Space>jd"] = {"<cmd>lua require('telescope.builtin').lsp_definitions(require('telescope.themes').get_ivy())<cr>", "Lsp 函数定义"},
-	["<Space>jD"] = {"<cmd>lua vim.lsp.buf.type_definition()<CR>", "Lsp 函数声明"},
-	["<Space>jr"] = {"<cmd>lua require('telescope.builtin').lsp_references(require('telescope.themes').get_ivy())<cr>", "Lsp 函数引用"},
-	["<Space>jt"] = {"<cmd>lua require('telescope.builtin').lsp_type_definitions(require('telescope.themes').get_ivy())<cr>", "Lsp type定义"},
-	["<Space>js"] = {"<cmd>execute printf('Leaderf gtags -s %s --top --auto-preview', expand('<cword>'))<cr>", "Leaderf symbol查询"},
-	["<Space>jo"] = {"<cmd>lua require('telescope.builtin').diagnostics(require('telescope.themes').get_ivy())<cr>", "Lsp 错误提示"},
-	["<Space>ja"] = {"<cmd>lua require('telescope.builtin').lsp_dynamic_workspace_symbols(require('telescope.themes').get_ivy())<cr>", "Lsp Symbols全局"},
-	["<Space>jf"] = {"<cmd>lua require('telescope.builtin').lsp_document_symbols(require('telescope.themes').get_ivy())<cr>", "Lsp Symbols 本文件"},
-	["<Space>jh"] = {"<cmd>lua require('telescope.builtin').help_tags()<cr>", "nvim 帮助"},
-
-	["<Space>jn"] = {"<cmd>lua vim.lsp.buf.rename()<CR>", "Lsp rename 变量"},
-	["<Space>jp"] = {"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "Lsp 路径显示"},
-
-	["<Space>g"] = { name = "telescope 查找内容和git" },
-	["<Space>gg"] = {"<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args(require('telescope.themes').get_ivy())<cr>", "查找内容"},
-	["<Space>gp"] = {"<cmd>GitGutterPrevHunk<CR>", "git 上个修改块"},
-	["<Space>gn"] = {"<cmd>GitGutterNextHunk<CR>", "git 下个修改块"},
-	["<Space>gh"] = {"<cmd>GitGutterLineHighlightsToggle<CR>", "git 高亮修改"},
-
-	--代码注释
-	["<Space>c"] = {"<Plug>kommentary_line_default", "代码注释"},
-
-	--telescope
-	["<C-u>"] = {"<cmd>lua require('telescope.builtin').lsp_document_symbols(require('telescope.themes').get_ivy())<cr>", "Find Document Symbols"},
-	["h"] = {"<cmd>GitGutterPreviewHunk<CR>", "git 修改块显示"},
-	["<C-r>"] = {"<cmd>lua require('undotree').toggle()<cr>", "undotree"},
-}
-
-local x_opts = {
-	mode = "x", -- x mode
-	prefix = "",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
-}
-
-local x_mappings = {
-	--代码注释
-	["<Space>c"] = {"<Plug>kommentary_visual_default", "代码注释"},
-}
-
-local leap_opts = {
-	mode = { "x", "n", "o"}, -- x,n,o mode
-	prefix = "",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
-}
-
-local leap_mappings = {
-	--代码注释
-	["s"] = {"<Plug>(leap-backward-to)", "Leap 向前查找"},
-	["<Space><Space>"] = {"<Plug>(leap-forward-to)", "Leap 向后查找,s向前查找"},
-	["<Space>s"] = {"<Plug>(leap-cross-window)", "Leap 全窗口跳转"},
-}
-
+-- 设置 which-key 并注册所有映射
 which_key.setup(setup)
-which_key.register(n_mappings, n_opts)
-which_key.register(x_mappings, x_opts)
-which_key.register(leap_mappings, leap_opts)
+which_key.add(all_mappings)
